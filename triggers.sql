0f0e243a55c101e6480821377d3a7f62ce5acfa0
MaijaHapponen <maija.happonen.96@gmail.com>
2017-02-09 14:52:56 +0100

    new triggaaah

--- triggers.sql
+++ triggers.sql
@@ -1,31 +1,62 @@
-CREATE OR REPLACE FUNCTION checkDuplicateRoad() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION checkRoad() RETURNS TRIGGER AS $$
     BEGIN
        IF((SELECT COUNT(*)
-       FROM Roads
+            FROM Roads
             WHERE ownerpersonnummer = NEW.ownerpersonnummer AND fromarea = NEW.toarea AND toarea = NEW.fromarea AND fromcountry = NEW.tocountry
             AND tocountry = NEW.fromcountry AND ownercountry = NEW.ownercountry) > 0)
             THEN RAISE EXCEPTION 'Road already exists';
        END IF;
+
+       IF((SELECT COUNT(*) 
+            FROM Persons
+            WHERE peronnummer = new.ownerpersonnummer AND country = new.ownercountry AND 
+            ((personLocationArea=new.fromarea AND personLocationCountry=new.fromcountry ) OR 
+                (personLocationArea=new.toarea AND personLocationCountry=new.tocountry))) < 1 )
+            THEN RAISE EXCEPTION 'Person not in right location';
+        ENDIF;
+
+        IF((SELECT budget
+            FROM Persons
+            WHERE peronnummer = new.ownerpersonnummer AND country = new.ownercountry) < getvalue('roadprice') )
+            THEN RAISE EXCEPTION 'Not enough money';
+        ENDIF;
+
     RETURN NEW;
     END
     $$ LANGUAGE 'plpgsql';
 
-CREATE TRIGGER roadDirection
+CREATE TRIGGER newRoad
     BEFORE INSERT ON Roads
     FOR EACH ROW
-    EXECUTE PROCEDURE checkDuplicateRoad();
+    EXECUTE PROCEDURE checkRoad();
 
 
+REATE OR REPLACE FUNCTION updateBudget() RETURNS TRIGGER AS $$
+    BEGIN
+        UPDATE Persons
+        SET budget = budget - getvalue('roadprice')
+        WHERE peronnummer = new.ownerperonnummer 
+        AND country = new.ownercountry;
+    RETURN NEW;
+    END
+    $$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER afterNewRoad
+    AFTER INSERT ON Roads
+    FOR EACH ROW
+    EXECUTE PROCEDURE updateBudget();
+
+
+/*This no work. Why??? */
 CREATE OR REPLACE FUNCTION removeDuplicate() RETURNS TRIGGER AS $$
     BEGIN
-        RAISE EXCEPTION 'asdf';
         IF(TG_OP = 'DELETE' ) THEN
         IF((SELECT COUNT(*)
         FROM Roads
             WHERE ownerpersonnummer = OLD.ownerpersonnummer AND fromarea = OLD.toarea AND toarea = OLD.fromarea AND fromcountry = OLD.tocountry
             AND tocountry = OLD.fromcountry AND ownercountry = OLD.ownercountry) > 0)
             THEN RAISE EXCEPTION 'asdf happend';
-       END IF;
+        END IF;
     END IF;
     RETURN OLD;
     END
@@ -35,3 +66,6 @@ CREATE TRIGGER removeRoad
     BEFORE DELETE ON Roads
     FOR EACH ROW
     EXECUTE PROCEDURE removeDuplicate();
+
+CREATE TRIGGER personLocation
+    BEFORE INSERT ON 
