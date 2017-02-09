1ec84a9e0a5369130812d9a6a3980de46536411e
MaijaHapponen <maija.happonen.96@gmail.com>
2017-02-09 17:48:21 +0100

    triggaah

---
+++ shouldWork.sql
@@ -0,0 +1,9 @@
+insert into areas values ('Sweden', 'Kungsbacka', 2);
+insert into areas values ('Sweden', 'Stockholm', 2);
+insert into areas values ('Sweden', 'Arvika', 2);
+
+
+insert into persons values ('Sweden', '19960123-2631', 'Frej Karlsson', 'Sweden', 'Gothenburg', '100000');
+	
+insert into roads values ('Sweden', 'Stockholm', 'Sweden', 'Arvika', 'Sweden', '19960123-2631', '1');
+insert into roads values ('Sweden', 'Gothenburg', 'Sweden', 'Arvika', 'Sweden', '19960123-2631', '1');
--- test.sql
+++ test.sql
@@ -1,9 +1,9 @@
 insert into areas values ('Sweden', 'Kungsbacka', 2);
 insert into areas values ('Sweden', 'Stockholm', 2);
 insert into areas values ('Sweden', 'Arvika', 2);
-insert into persons values ('Sweden', '19960123-2631', 'Frej Karlsson', 'Sweden', 'Gothenburg', '100000');
-insert into roads values ('Sweden', 'Stockholm', 'Sweden', 'Arvika', 'Sweden', '19960123-2631', '1');
-insert into roads values ('Sweden', 'Gothenburg', 'Sweden', 'Arvika', 'Sweden', '19960123-2631', '1');
+insert into persons values ('Sweden', '19960123-2631', 'Frej Karlsson', 'Sweden', 'Gothenburg', 10);
+insert into roads values ('Sweden', 'Stockholm', 'Sweden', 'Arvika', 'Sweden', '19960123-2631', 1);
+insert into roads values ('Sweden', 'Gothenburg', 'Sweden', 'Arvika', 'Sweden', '19960123-2631', 1);
 
 
 select * from roads;
@@ -11,3 +11,6 @@ update roads
 set roadtax = 2, toarea = 'Stockholm';
 select * from roads;
 select * from persons;
+
+insert into cities values('Sweden', 'Gothenburg', 10);
+insert into hotels values('Sunny Beach', 'Sweden', 'Gothenburg', 'Sweden', '19960123-2631');
