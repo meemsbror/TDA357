7002741b4671c3be743dd467d56613e587b24ad0
Frej Karlsson <frejlekarlsson@gmail.com>
2017-02-13 23:13:43 +0100

    now deducts properly when traveling road

--- persontriggers.sql
+++ persontriggers.sql
@@ -28,10 +28,10 @@ CREATE OR REPLACE FUNCTION updatePerson() RETURNS TRIGGER AS $$
                 WHERE ((fromarea = old.locationarea AND fromcountry = old.locationcountry
                 AND toarea = new.locationarea AND tocountry = new.locationcountry)
                 OR (fromarea = new.locationarea AND fromcountry = new.locationcountry
-                AND toarea = old.locationarea AND tocountry = old.locationcountry)))
-                < new.budget)
-                LIMIT 1 THEN
-                RAISE EXCEPTION 'yoyo';
+                AND toarea = old.locationarea AND tocountry = old.locationcountry))
+                ORDER BY roadtax ASC
+                LIMIT 1
+                ) < new.budget) THEN
                 RETURN NEW;
             END IF;
 
@@ -49,4 +49,39 @@ CREATE TRIGGER beforePerUpdate
     FOR EACH ROW
     EXECUTE PROCEDURE updatePerson();
 
+CREATE OR REPLACE FUNCTION afterUpdatePerson() RETURNS TRIGGER AS $$
+    BEGIN
+    /* Check if there is a free road between the locations */
+    IF((SELECT COUNT(*)
+        FROM Roads
+        WHERE ((fromarea = old.locationarea AND fromcountry = old.locationcountry
+        AND toarea = new.locationarea AND tocountry = new.locationcountry)
+        OR (fromarea = new.locationarea AND fromcountry = new.locationcountry
+        AND toarea = old.locationarea AND tocountry = old.locationcountry))
 
+        AND ((ownerpersonnummer = new.personnummer AND ownercountry = new.country)
+        OR ((ownerpersonnummer = ' ' AND ownercountry = ' ')))) > 0) THEN
+
+        RETURN NEW;
+    END IF;
+
+    /* Deducts cheapest alternative from budget */
+    UPDATE persons
+    SET budget = budget - 
+    (SELECT cost
+    FROM NextMoves
+    WHERE country = old.locationcountry AND area = old.locationarea
+    AND destcountry = new.locationcountry AND destarea = new.locationarea
+    ORDER BY cost ASC
+    LIMIT 1)
+    WHERE personnummer = new.personnummer AND country = new.country;
+
+    RETURN NEW;
+END
+$$ LANGUAGE 'plpgsql';
+
+CREATE TRIGGER afterUpdate
+    AFTER UPDATE OF locationArea, locationCountry
+    ON Persons
+    FOR EACH ROW
+    EXECUTE PROCEDURE afterUpdatePerson();
