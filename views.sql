9e9f029e1d0eac1da72a491aa470625ec2454aac
Frej Karlsson <frejlekarlsson@gmail.com>
2017-02-14 11:15:18 +0100

    yay

--- views.sql
+++ views.sql
@@ -1,11 +1,11 @@
 
 CREATE OR REPLACE VIEW  NextMoves AS
-SELECT p.country AS personcountry, 
+SELECT p.country AS personcountry,
 p.personnummer,
-p.locationcountry AS country, 
+p.locationcountry AS country,
 p.locationarea AS area,
-r.tocountry AS destcountry, 
-r.toarea AS destarea, 
+r.tocountry AS destcountry,
+r.toarea AS destarea,
 r.roadtax AS cost
 
 FROM Persons p, Roads r
@@ -13,35 +13,17 @@ WHERE (p.locationcountry = r.fromcountry AND p.locationarea = r.fromarea)
 
 UNION ALL
 
-SELECT p.country AS personcountry, 
+SELECT p.country AS personcountry,
 p.personnummer,
-p.locationcountry AS country, 
+p.locationcountry AS country,
 p.locationarea AS area,
-r.fromcountry AS destcountry, 
-r.fromarea AS destarea, 
+r.fromcountry AS destcountry,
+r.fromarea AS destarea,
 r.roadtax AS cost
 FROM Persons p, Roads r
 WHERE (p.locationcountry = r.tocountry AND p.locationarea = r.toarea)
 ;
 
-/*
-CREATE OR REPLACE VIEW HotelAssets AS
-	SELECT 	ownercountry, 
-			ownerpersonnummer, 
-			COUNT(*) * getval('hotelprice') AS assets, 
-			COUNT(*) * getval('hotelrefund') * getval('hotelprice') AS reclaimable
-	FROM Hotels
-	GROUP BY ownercountry, ownerpersonnummer
-	;
-
-CREATE OR REPLACE VIEW RoadAssets AS
-	SELECT 	ownercountry, 
-			ownerpersonnummer, 
-			COUNT(*) * getval('roadprice') AS assets
-	FROM Roads
-	GROUP BY ownercountry, ownerpersonnummer
-	;
-*/
 
 CREATE OR REPLACE VIEW AssetsSummary AS
 WITH
@@ -59,7 +41,7 @@ WITH
     personBudget AS(
     	SELECT personnummer AS ownerpersonnummer, country AS ownercountry, budget
     	FROM Persons)
-SELECT ownercountry, ownerpersonnummer, hAssets + rAssets AS assets, reclaimable, budget
+SELECT ownercountry AS country, ownerpersonnummer AS personnummer, budget, hAssets + rAssets AS assets, reclaimable
 FROM roadAssets r
 NATURAL INNER JOIN hotelAssets h
 NATURAL INNER JOIN personBudget p;
